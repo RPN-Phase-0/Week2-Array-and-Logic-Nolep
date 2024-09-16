4bd111100988e69b64a09c8de732ea27218a890e
Wiguna Tedja <gustedja123@gmail.com>
2024-09-16 14:01:56 +0800

    jawab soal 3 ujian

--- quiz/ujian/soal3.js
+++ quiz/ujian/soal3.js
@@ -3,8 +3,27 @@ diberikan sebuah function groupAnimals(animals) yang menerima satu parameter ber
 fungsi ini akan me-return array 2 dimensi
 */
 function groupAnimals(animals) {
+    let sortedGroups = []
+    //sort
     let sortedAnimals = animals.sort()
-    return sortedAnimals
+
+    //
+
+    //looping pengelompokan
+    for (let i = 97; i < 123; i++) {
+        let abjadAwal = String.fromCharCode(i);
+
+        let tempGroup = []
+        for (let j = 0; j < sortedAnimals.length; j++) {
+            if (sortedAnimals[j].startsWith(abjadAwal)){
+                tempGroup.push(sortedAnimals[j])
+            };
+        }
+        if (tempGroup.length != 0) {
+            sortedGroups.push(tempGroup)            
+        }
+    }
+    return sortedGroups
   }
   
   // TEST CASES
