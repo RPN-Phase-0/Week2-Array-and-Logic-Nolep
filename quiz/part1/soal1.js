3e298009dc8d878381f1a49b0898c1bfb44ac2f2
ihsan <75242608+ihsanoo@users.noreply.github.com>
2024-09-20 07:00:01 +0800

    yanzah

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,50 @@
+/*
+Buatlah sebuah fungsi dengan nama dataHandling dengan sebuah parameter untuk menerima argumen. Argumen yang akan diterima adalah sebuah array yang berisi beberapa array sejumlah n.
+Tugas kamu adalah mengimplementasikan fungsi dataHandling agar dapat menampilkan data-data pada dari argumen seperti di bawah ini:
+*/
+let input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+];
+
+function dataHandling(data) {
+    let result = "";
+    for (let i = 0; i < data.length; i++) {
+        result += "Nomor ID : " + data[i][0] + '\n';
+        result += "Nama Lengkap : " + data[i][1] + '\n';
+        result += "TTL : " + data[i][2] +", " + data[i][3] + '\n';
+        result += "Hobi : " + data[i][4] + '\n';
+        result += "\n";
+    }
+
+    return result;
+}
+
+
+
+console.log(dataHandling(input));
+/*
+output yang diharapkan
+
+Nomor ID:  0001
+Nama Lengkap:  Roman Alamsyah
+TTL:  Bandar Lampung 21/05/1989
+Hobi:  Membaca
+
+Nomor ID:  0002
+Nama Lengkap:  Dika Sembiring
+TTL:  Medan 10/10/1992
+Hobi:  Bermain Gitar
+
+Nomor ID:  0003
+Nama Lengkap:  Winona
+TTL:  Ambon 25/12/1965
+Hobi:  Memasak
+
+Nomor ID:  0004
+Nama Lengkap:  Bintang Senjaya
+TTL:  Martapura 6/4/1970
+Hobi:  Berkebun
+*/
