f7055d044432ab923e7d38ffd95d84a210d301c3
San <184384788+Stellarrealm@users.noreply.github.com>
2024-12-05 09:24:15 +0700

    part 3

---
+++ quiz/part3/soal3-arithmetic-progression.js
@@ -0,0 +1,18 @@
+function tentukanDeretAritmatika(arr) {
+    const commonDifference = arr[1] - arr[0];
+
+    for (let i = 2; i < arr.length - 1; i++) {
+        if (arr[i + 1] - arr[i] !== commonDifference) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// TEST CASES
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
