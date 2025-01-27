863dfea91184713bf4b4125e929aeb03d397da83
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-01-27 07:13:26 +0700

    up soal-3

---
+++ quiz/part2/soal-4.js
@@ -0,0 +1,17 @@
+function pasanganTerbesar(num) {
+  for (i=0; i<=num.length; i++){
+    
+  }
+  }
+  
+  // TEST CASES
+  console.log(pasanganTerbesar(641573)); // 73
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
+
+//64>41>15<57<73
+/*
+
+*/
