78e42858899dc118867f072ee5c83e446cb701e8
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-01-29 00:05:17 +0700

    update soal 3 & 4

--- quiz/part2/soal-4.js
+++ quiz/part2/soal-4.js
@@ -1,9 +1,19 @@
 function pasanganTerbesar(num) {
-  for (i=0; i<=num.length; i++){
-    
+  let arrNum = [];
+  let hasil;
+  let stringNum = num.toString();
+
+  for(let i=0; i<stringNum.length; i++){
+    if (i < stringNum.length-1){
+      arrNum.push(stringNum[i] + stringNum[i+1]);
+    }
   }
-  }
-  
+
+  arrNum.sort(function(num1, num2){
+    return num2-num1;
+  })
+  return parseInt(arrNum[0])
+}
   // TEST CASES
   console.log(pasanganTerbesar(641573)); // 73
   console.log(pasanganTerbesar(12783456)); // 83
@@ -11,7 +21,8 @@ function pasanganTerbesar(num) {
   console.log(pasanganTerbesar(71856421)); // 85
   console.log(pasanganTerbesar(79918293)); // 99
 
-//64>41>15<57<73
 /*
+arr.sort(function(value1, value2) { return Number(value1) < Number(value2) });
 
-*/
+
+*/  
