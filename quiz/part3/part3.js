921ae7c7d286323d043a9bb6665a332a6aa01623
Hanif Fauzi <haniffauzihakim@gmail.com>
2025-02-09 23:49:16 +0700

    Hanif

--- quiz/part3/part3.js
+++ quiz/part3/part3.js
@@ -1,20 +1,20 @@
 //soal no 1
 
-// function cariMean(arr) {
+function cariMean(arr) {
   
-//   let jumlah = 0
+  let jumlah = 0
 
-//   for (let i = 0; i < arr.length; i++){
-//     jumlah += arr[i]
-//   }
-//   let hasilMean = Math.round(jumlah / arr.length)
-//   return hasilMean
-// }
-// console.log(cariMean([1, 2, 3, 4, 5])); // 3
-// console.log(cariMean([3, 5, 7, 5, 3])); // 5
-// console.log(cariMean([6, 5, 4, 7, 3])); // 5
-// console.log(cariMean([1, 3, 3])); // 2
-// console.log(cariMean([7, 7, 7, 7, 7])); // 7
+  for (let i = 0; i < arr.length; i++){
+    jumlah += arr[i]
+  }
+  let hasilMean = Math.round(jumlah / arr.length)
+  return hasilMean
+}
+console.log(cariMean([1, 2, 3, 4, 5])); // 3
+console.log(cariMean([3, 5, 7, 5, 3])); // 5
+console.log(cariMean([6, 5, 4, 7, 3])); // 5
+console.log(cariMean([1, 3, 3])); // 2
+console.log(cariMean([7, 7, 7, 7, 7])); // 7
 
 
 //no soal 2
