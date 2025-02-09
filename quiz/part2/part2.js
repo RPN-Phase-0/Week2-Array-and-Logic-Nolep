55ebcd7ff7f396f8b48cd7b7172a8c2793adb512
Hanif Fauzi <haniffauzihakim@gmail.com>
2025-02-09 16:30:10 +0700

    Hanif

--- quiz/part2/part2.js
+++ quiz/part2/part2.js
@@ -1,58 +1,58 @@
-// //soal no 1
+//soal no 1
 
-// function palindrome(kata) {
+function palindrome(kata) {
 
-//   let reverseString = kata.split('').reverse().join('')
+  let reverseString = kata.split('').reverse().join('')
   
-//   if (kata === reverseString) {
-//     return true
-//   } else {
-//     return false
-//   }
-// }
+  if (kata === reverseString) {
+    return true
+  } else {
+    return false
+  }
+}
 
-// console.log(palindrome('katak')); // true
-// console.log(palindrome('blanket')); // false
-// console.log(palindrome('civic')); // true
-// console.log(palindrome('kasur rusak')); // true
-// console.log(palindrome('mister')); // false
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
 
-// //soal no 2
+//soal no 2
 
-// function angkaPalindrome(num) {
-//   let numStr = num.toString()
-//   let reverse = numStr.split('').reverse().join('');
-//   while (numStr !== reverse || num <= 10) {  
-//     num++;
-//     let numStr = num.toString()
-//     let reverse = numStr.split('').reverse().join('');
-//     if (numStr === reverse) {
-//       return num
-//     }
-//   }
+function angkaPalindrome(num) {
+  let numStr = num.toString()
+  let reverse = numStr.split('').reverse().join('');
+  while (numStr !== reverse || num <= 10) {  
+    num++;
+    let numStr = num.toString()
+    let reverse = numStr.split('').reverse().join('');
+    if (numStr === reverse) {
+      return num
+    }
+  }
   
-// }
-// console.log(angkaPalindrome(8)); // 9
-// console.log(angkaPalindrome(10)); // 11
-// console.log(angkaPalindrome(117)); // 121
-// console.log(angkaPalindrome(175)); // 181
-// console.log(angkaPalindrome(1000));
+}
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000));
 
 
-// //soal no 3
+//soal no 3
 
-// function hitungJumlahKata(kalimat) {
-//   let jumlahKata = kalimat.split(' ')
+function hitungJumlahKata(kalimat) {
+  let jumlahKata = kalimat.split(' ')
 
 
-//   return jumlahKata.length
-// }
+  return jumlahKata.length
+}
 
-// console.log(hitungJumlahKata('I have a dream')); // 4
-// console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
-// console.log(hitungJumlahKata('A song to sing')); // 4
-// console.log(hitungJumlahKata('I')); // 1
-// console.log(hitungJumlahKata('I believe I can code')); // 5
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
 
 //soal no 4
 
