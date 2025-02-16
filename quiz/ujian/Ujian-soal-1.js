632b9862823f0aecf30fbf1a77a345341944ddd5
Irsannerha <44488538+yaaelahsan@users.noreply.github.com>
2025-02-16 13:35:11 +0700

    Upload Week2

---
+++ quiz/ujian/Ujian-soal-1.js
@@ -0,0 +1,28 @@
+/*
+Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0.
+*/
+function targetTerdekat(arr) {
+    let posisiO = arr.indexOf('o');
+    if (posisiO === -1)
+        return 0;
+
+    let jarakDekat = Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'x') {
+            let jarak = Math.abs(i - posisiO);
+            if (jarak < jarakDekat) {
+                jarakDekat = jarak;
+            }
+        }
+    }
+    return jarakDekat === Infinity ? 0 : jarakDekat;
+  }
+  
+  // TEST CASES
+  console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+  console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+  console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+  console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+  console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+  console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', 'o', ' ', 'x'])); // 1
