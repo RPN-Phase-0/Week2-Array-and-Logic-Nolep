fb15b9c3c1b75e15952e4afff92f95b2740c2895
Alpharii <alphari404@gmail.com>
2025-02-21 09:51:42 +0700

    part 1 finish

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,42 @@
+let input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
+
+function dataHandling(input) {
+    input.forEach(element => {
+        console.log('Nomor ID: ', element[0]);
+        console.log('Nama Lengkap: ', element[1]);
+        console.log('TTL: ', element[2], element[3]);
+        console.log('Hobi: ', element[4]);
+        console.log();
+    });
+}
+
+/*
+output yang diharapkan
+
+Nomor ID:  0001
+Nama Lengkap:  Roman Alamsyah
+TTL:  Bandar Lampung 21/05/1989
+Hobi:  Membaca
+
+Nomor ID:  0002
+Nama Lengkap:  Dika Sembiring
+TTL:  Medan 10/10/1992
+Hobi:  Bermain Gitar
+
+Nomor ID:  0003
+Nama Lengkap:  Winona
+TTL:  Ambon 25/12/1965
+Hobi:  Memasak
+
+Nomor ID:  0004
+Nama Lengkap:  Bintang Senjaya
+TTL:  Martapura 6/4/1970
+Hobi:  Berkebun
+*/
+
+console.log(dataHandling(input));
