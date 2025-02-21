423613068086a332c908d8ceb19b3dde6ae425b2
Sigrajavascript1717 <sigra170800@gmail.com>
2025-02-21 17:40:00 +0700

    ulang

--- quiz/part1/soal2.js
+++ quiz/part1/soal2.js
@@ -9,35 +9,33 @@ let input = [
 function dataHandling(input) {
   input.splice(1, 1, "Roman Alamsyah Elsharawy");
   input.splice(2, 1, "Provinsi Bandar Lampung");
-
-  let tanggalLahir = input[3].split("/");
-  let bulan = [
-    "Januari",
-    "Februari",
-    "Maret",
-    "April",
-    "Mei",
-    "Juni",
-    "Juli",
-    "Agustus",
-    "September",
-    "Oktober",
-    "November",
-    "Desember",
-  ];
-  let namaBulan = bulan[parseInt(tanggalLahir[1]) - 1];
-
-  // 3. Membuat format tanggal lahir
-  let tanggalLahirFormat = tanggalLahir.join("-");
-
-  // 4. Memotong nama
-  let namaPotong = input[1].slice(0, 15);
-
+  input.splice(4, 0, "Pria ");
+  input.splice(5, 0, "SMA Internasional Metro");
+  let tanggal = input[3].split("/");
+  let bulan = tanggal[1];
+  let namabulan = "";
+  if (bulan) {
+    namabulan += "mei";
+  } else {
+    namabulan += null;
+  }
+  let reverse = `${tanggal[2]}-${tanggal[0]}-${tanggal[1]}`;
+  let karakter = input[1].slice(0, 15);
   console.log(input);
-  console.log(namaBulan);
-  console.log(tanggalLahir);
-  console.log(tanggalLahirFormat);
-  console.log(namaPotong);
+  console.log(namabulan);
+  console.log(tanggal);
+  console.log(reverse);
+  console.log(karakter);
 }
 
 dataHandling(input);
+
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah //batasi hanya 15 karakter saja pada array elemen ke 2
+ */
