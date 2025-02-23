d4b80429bf0524b372a280fdad7d143f8f305fc6
ulum <apumm12@gmail.com>
2025-02-23 15:11:36 +0700

    soal1 part1

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,27 @@
+let input = [
+  ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+  ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+  ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+  ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"],
+];
+function dataHandling(n) {
+  for (let i = 0; i < n.length; i++) {
+    for (let j = 0; j < n[i].length; j++) {
+      switch (j) {
+        case 0:
+          console.log(`Nomor ID: ${n[i][j]}`);
+          break;
+        case 1:
+          console.log(`Nama Lengkap: ${n[i][j]}`);
+          break;
+        case 2:
+          console.log(`TTL: ${n[i][j]}, ${n[i][j + 1]}`);
+          break;
+        case 4:
+          console.log(`Hobi: ${n[i][j]}`);
+          break;
+      }
+    }
+  }
+}
+dataHandling(input);
