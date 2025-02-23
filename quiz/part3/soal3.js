50931ebbadba01f66d6dae2cebfdd6a9b8466883
ulum <apumm12@gmail.com>
2025-02-23 22:38:03 +0700

    anjaayy deretgeometri T_T

--- quiz/part3/soal3.js
+++ quiz/part3/soal3.js
@@ -1,18 +1,16 @@
 function tentukanDeretAritmatika(arr) {
-  if (arr.length < 2) {
-    return false;
-  } else {
-    let beda = arr[1] - arr[0];
-    for (let i = 1; i < arr.length - 1; i++) {
-      if (arr[i + 1] - arr[i] !== beda) {
-        return false;
-      }
+  let beda = arr[1] - arr[0];
+  for (let i = 1; i < arr.length - 1; i++) {
+    // console.log(beda);
+    if (arr[i + 1] - arr[i] !== beda) {
+      return false;
+    } else {
+      return true;
     }
-    return true;
   }
 }
 
-// TEST CASES
+/// TEST CASES
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
 console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
 console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
---
+++ quiz/part3/soal4.js
@@ -0,0 +1,20 @@
+function tentukanDeretGeometri(arr) {
+  let rasio = arr[1] / arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    // beda = arr[i + 1] / arr[i];
+    // console.log(beda);
+    if (arr[i + 1] / arr[i] !== rasio) {
+      // console.log("false");
+      return false;
+    }
+  }
+  // console.log("true");
+  return true;
+}
+
+// TEST CASES
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
