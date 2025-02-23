358ff482cca58a1fed374e106c2c40e796d9896e
ulum <apumm12@gmail.com>
2025-02-23 20:12:19 +0700

    soal1 part2

---
+++ quiz/part2/soal1.js
@@ -0,0 +1,16 @@
+const palindrome = (kata) => {
+  let word = kata;
+  kata = kata.split("");
+  kata.reverse();
+  kata = kata.join("");
+  if (kata == word) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(palindrome("katak")); // true
+console.log(palindrome("blanket")); // false
+console.log(palindrome("civic")); // true
+console.log(palindrome("kasur rusak")); // true
+console.log(palindrome("mister")); // false
