3ceee0c80e8ee63f0f439f31df2d939bddd0029d
Alpharii <alphari404@gmail.com>
2025-02-24 13:50:37 +0700

    part 3 selesai

---
+++ quiz/part3/soal3.js
@@ -0,0 +1,18 @@
+//tentukan apakah ini deret aritmatika atau bukan
+function tentukanDeretAritmatika(arr) {
+    let rasio = arr[1] - arr[0]
+    let secondRasio = arr[arr.length - 1] - arr[arr.length - 2]
+
+    if(rasio === secondRasio){
+        return true
+    } else {
+        return false
+    }
+}
+
+// TEST CASES
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
