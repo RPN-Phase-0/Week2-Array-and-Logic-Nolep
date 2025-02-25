b72a9810ff43268a18a478adad42bda8c24b7fbd
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-02-25 20:09:54 +0700

    Update tugas part 2 soal pertama (mode izin serius)

--- quiz/part2/soal-1.js
+++ quiz/part2/soal-1.js
@@ -8,26 +8,14 @@ Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak
 
 function palindrome(kata) {
     // you can only write your code here!
-    let isPalindrome;
+    for(let i = 0; i < kata.length; i++) {
+        let j = kata.length - 1 - i;
 
-    for (let i = 0; i < kata.length; i++) {
-        let berotakSenku = i + 1;
-        for (let j = kata.length - berotakSenku; j >= i; j--) {
-            if (kata[i] === kata[j]) {
-                console.log("loop " + i)
-                console.log("i: " + kata[i] + " " + "j: " + kata[j])
-                isPalindrome = true;
-                console.log(isPalindrome);
-                break;
-            } else {
-                console.log("loop " + i)
-                console.log("i: " + kata[i] + " " + "j: " + kata[j])
-                isPalindrome = false;
-                return isPalindrome
-            }
+        if (kata[i] !== kata[j]) {
+            return false;
         }
     }
-    return isPalindrome;
+    return true;
 }
   
 // TEST CASES
@@ -36,8 +24,9 @@ console.log(palindrome('blanket')); // false
 console.log(palindrome('civic')); // true
 console.log(palindrome('kasur rusak')); // true
 console.log(palindrome('mister')); // false
-console.log(palindrome('berotaksenku'));
-console.log(palindrome('berotaksenkuukneskatoreb'));
+console.log(palindrome('berotaksenku')); // false
+console.log(palindrome('berotaksenkub')); // false
+console.log(palindrome('berotaksenkuukneskatoreb')); // true
 
 
   
