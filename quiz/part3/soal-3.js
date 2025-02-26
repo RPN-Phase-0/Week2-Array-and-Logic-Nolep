c109b7fb655294ad8dc124bbcc7c79d3c0829f96
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-02-26 14:25:38 +0700

    Submit tugas part 3 soal ke-empat dan fix bug soal ke-tiga

--- quiz/part3/soal-3.js
+++ quiz/part3/soal-3.js
@@ -9,12 +9,12 @@ function tentukanDeretAritmatika(arr) {
     let bil2 = arr[i - 1]
     console.log(bil2);
     
-    if (bil1 - bil2 === selisih) {
-        return true
-    } else {
+    if (bil1 - bil2 !== selisih) {
         return false
-    }
+    } 
   }
+
+  return true
 }
 
 // TEST CASES
@@ -23,4 +23,4 @@ console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
 console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
 console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
-console.log(tentukanDeretAritmatika([3, 6, 9, 12])); // true
+console.log(tentukanDeretAritmatika([3, 6, 9, 12])); // tes -- true
