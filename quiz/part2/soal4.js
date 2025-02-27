b75722de735c9bc0c373578603a3f124320a9e17
hargo-xyz <kharismahargo@gmail.com>
2025-02-27 10:07:35 +0700

    part 2

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,20 @@
+function pasanganTerbesar(num) {
+    // you can only write your code here!
+    let str = num.toString();
+    let bebas = 0;
+
+    for( let i = 0; i < str.length; i++){
+        let urut = str[i] + str[i + 1];
+        if( urut > bebas){
+            bebas = urut;
+        }
+    }
+return bebas
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
