6d6accd3a4503ceb79f80b93ddd2cd82fac470fb
zxcvbnm0091 <nisrinaadil45@gmail.com>
2025-03-03 19:38:56 +0700

    Submit tugas pertama

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,47 @@
+// Soal 1
+/*
+Buatlah sebuah fungsi dengan nama dataHandling dengan sebuah parameter untuk menerima argumen. Argumen yang akan diterima adalah sebuah array yang berisi beberapa array sejumlah n.
+Tugas kamu adalah mengimplementasikan fungsi dataHandling agar dapat menampilkan data-data pada dari argumen seperti di bawah ini:
+*/
+let input = [
+                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+            ]
+
+/*
+output yang diharapkan
+
+Nomor ID:  0001
+Nama Lengkap:  Roman Alamsyah
+TTL:  Bandar Lampung 21/05/1989
+Hobi:  Membaca
+
+Nomor ID:  0002
+Nama Lengkap:  Dika Sembiring
+TTL:  Medan 10/10/1992
+Hobi:  Bermain Gitar
+
+Nomor ID:  0003
+Nama Lengkap:  Winona
+TTL:  Ambon 25/12/1965
+Hobi:  Memasak
+
+Nomor ID:  0004
+Nama Lengkap:  Bintang Senjaya
+TTL:  Martapura 6/4/1970
+Hobi:  Berkebun
+*/
+
+// Write your code here
+const dataHandling = (users) => {
+    input.forEach((user) => {
+        console.log(`Nomor ID:  ${data[0]}`);
+        console.log(`Nama Lengkap:  ${data[1]}`);
+        console.log(`TTL:  ${data[2]} ${data[3]}`);
+        console.log(`Hobi:  ${data[4]} \n`);
+    })
+}
+
+dataHandling(input);
