a1fdd91ce63bc43c6bb862f2de31ccb8ce07b2bf
prismaaryadwi <prisma.aryadwi@gmail.com>
2025-03-07 15:07:55 +0700

    soal2

---
+++ quiz/ujian/soal2.js
@@ -0,0 +1,30 @@
+//Function akan me-return array multidimensi dimana array tersebut berisikan 3 kategori/kelompok:
+
+//kelompok pertama (baris pertama) merupakan angka-angka genap
+//kelompok ke-2 (baris ke-2) merupakan angka-angka ganjil
+//kelompok ke-3 (baris ke-3) merupakan angka-angka kelipatan 3
+//Contoh jika arr inputan adalah [45, 20, 21, 2, 7] maka output: [ [ 20, 2 ], [ 7 ], [ 45, 21 ] ]
+
+function mengelompokkanAngka(arr) {
+    let genap = [];//array genap
+    let ganjil = [];//array ganjil
+    let kelipatan3 = [];//array kelipatan3
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 3 === 0) {
+            kelipatan3.push(arr[i]);//jika kelipatan tiga masuk ke kelipatan3
+        } else if (arr[i] % 2 === 0) {
+            genap.push(arr[i]);//jika genap masuk ke genap
+        } else {
+            ganjil.push(arr[i]);//jika ganjil masuk ke ganjil
+        }
+    }
+
+    return [genap, ganjil, kelipatan3];
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
