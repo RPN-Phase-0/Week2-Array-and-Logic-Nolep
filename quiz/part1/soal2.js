7ffe9481c15447ecf442cdb41011dd75125bba35
Kratione <molaingame@gmail.com>
2025-03-08 18:10:22 +0700

    Submit Quiz Week-2

--- quiz/part1/soal2.js
+++ quiz/part1/soal2.js
@@ -3,7 +3,7 @@
 
 let input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
 
-function dataHandling2(arr) {
+function dataHandling2(input) {
     input.splice(1, 2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung");
     input.splice(4, 1, "Pria");
     input.push("SMA Internasional Metro");
